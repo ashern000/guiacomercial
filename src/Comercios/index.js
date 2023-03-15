cf75d89141c4ad1a009c0eac9236bc6e21b9885c
joaosied <105009001+joaosied@users.noreply.github.com>
2023-03-15 17:23:54 -0300

    Add files via upload

--- src/Comercios/index.js
+++ src/Comercios/index.js
@@ -1,13 +1,43 @@
-import { ComercioHeader, Banner } from "./styles"
+import { ComercioHeader, Banner, ImgPerfil, Stars, AddFav, ClassificationDiv, DescriptionComercio, InformationDiv } from "./styles";
+import { AiOutlineStar, AiOutlineHeart } from "react-icons/ai"
 
-export default function Comercios(){
-    return(
+export default function Comercios() {
+    return (
         <>
-        <ComercioHeader>
-            <Banner>
+            <ComercioHeader>
 
-            </Banner>
-        </ComercioHeader>
+                <Banner>
+                    <ImgPerfil />
+                </Banner>
+
+                <AddFav>
+                    <AiOutlineHeart size={30} />
+                    <p>Adicionar local aos Favoritos</p>
+                </AddFav>
+
+                <ClassificationDiv>
+                    <Stars>
+                        <AiOutlineStar size={25} />
+                        <AiOutlineStar size={25} />
+                        <AiOutlineStar size={25} />
+                        <AiOutlineStar size={25} />
+                        <AiOutlineStar size={25} />
+                    </Stars>
+
+                </ClassificationDiv>
+
+                <InformationDiv>
+                <DescriptionComercio>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dolor tortor, dictum et arcu id, mollis accumsan tortor.
+                        Sed a nibh ut urna porttitor volutpat sed eget mi. In fringilla, est in convallis tincidunt, nulla metus blandit massa,
+                        ut feugiat ex risus id magna. Nunc malesuada, nunc quis ultrices volutpat, dolor odio porta ex, vel convallis diam metus
+                        nec nibh. Sed vel porta erat, eu varius sem. Phasellus sed dapibus nunc. Curabitur vehicula lorem porttitor, aliquet
+                        eros lacinia, hendrerit massa. Morbi at ante id sapien scelerisque pulvinar non quis leo.
+                    </p>
+                </DescriptionComercio>
+                </InformationDiv>
+            </ComercioHeader>
         </>
     )
 }
--- src/Comercios/styles.js
+++ src/Comercios/styles.js
@@ -1,8 +1,22 @@
 import styled from "styled-components";
 import imgBanner from "./img/spani.jpg"
+import imgPerfil from   "./img/spani.png"
 
 export const ComercioHeader = styled.div`
 
+`
+
+export const ImgPerfil = styled.div`
+width: 200px;
+height: 200px;
+border: 1px solid black;
+border-radius: 100px;
+position: relative;
+bottom: -100px;
+background-image: url(${imgPerfil});
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;
 `
 
 export const Banner = styled.div`
@@ -12,4 +26,51 @@ background-image: url(${imgBanner});
 background-repeat: no-repeat;
 background-size: cover;
 background-position: center;
+display: flex;
+justify-content: center;
+align-items: flex-end;
+`
+
+export const Stars = styled.div`
+width: 50%;
+height: 14vh;
+display: flex;
+justify-content: center;
+align-items:flex-end;
+`
+
+export const ClassificationDiv = styled.div`
+display: flex;
+justify-content: center;
+`
+
+export const AddFav = styled.div`
+display: flex;
+align-items: center;
+justify-content: flex-end;
+position: relative;
+right: 20vh;
+font-size: 20px;
+padding-top: 5px;
+
+p{
+    padding-left: 20px;
+}
+`
+
+export const InformationDiv = styled.div`
+display: flex;
+width: 100%;
+justify-content: center;
+align-items: center;
+margin-top: 3vh;
+`
+
+export const DescriptionComercio = styled.div`
+display: flex;
+font-size: 18px;
+align-items: center;
+width: 50%;
+height: 20vh;
+text-align: center;
 `
