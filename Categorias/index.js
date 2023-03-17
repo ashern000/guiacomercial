a919a78abc492bc3ede8ccd9614888dee83c1ffa
joaosied <105009001+joaosied@users.noreply.github.com>
2023-03-17 17:19:55 -0300

    Add files via upload

---
+++ Categorias/index.js
@@ -0,0 +1,85 @@
+import {
+  SearchStyled,
+  ButtonStyled,
+  SectionSearch,
+  FilterSection,
+  BoxCategories1, BoxCategories2, BoxCategories3, BoxCategories4,
+  ItemCategory,
+} from "./styles";
+import { FiSearch } from "react-icons/fi";
+import { BiFilterAlt } from "react-icons/bi";
+import Footer from "../components/Footer";
+
+export default function Categorias() {
+  return (
+    <>
+      <SectionSearch>
+        <h1>Todas as Categorias</h1>
+        <SearchStyled>
+          <form>
+            <input type="text" placeholder="Pesquise por Categorias" />
+            <ButtonStyled type="submit">
+              <FiSearch size={20} />
+            </ButtonStyled>
+          </form>
+        </SearchStyled>
+        <FilterSection>
+          <BiFilterAlt size={20} />
+          <p>Classificar por: Mais Procurados</p>
+        </FilterSection>
+      </SectionSearch>
+
+      {/* first box to cards */}
+      <BoxCategories1>
+        <ItemCategory></ItemCategory>
+
+        <ItemCategory></ItemCategory>
+
+        <ItemCategory></ItemCategory>
+
+        <ItemCategory></ItemCategory>
+
+      </BoxCategories1>
+
+    {/* second box to cards */}
+      <BoxCategories2>
+        <ItemCategory> 
+        <FiSearch size={20} />
+        </ItemCategory>
+
+        <ItemCategory></ItemCategory>
+
+        <ItemCategory></ItemCategory>
+
+        <ItemCategory></ItemCategory>
+
+      </BoxCategories2>
+
+    {/* third box to cards */}
+      <BoxCategories3>
+        <ItemCategory></ItemCategory>
+
+        <ItemCategory></ItemCategory>
+
+        <ItemCategory></ItemCategory>
+
+        <ItemCategory></ItemCategory>
+
+      </BoxCategories3>
+
+    {/* fourth box to cards */}
+      <BoxCategories4>
+        <ItemCategory></ItemCategory>
+
+        <ItemCategory></ItemCategory>
+
+        <ItemCategory></ItemCategory>
+
+        <ItemCategory></ItemCategory>
+
+      </BoxCategories4>
+      
+      <Footer/>
+    </>
+  );
+}
---
+++ SubCategorias/index.js
@@ -0,0 +1,74 @@
+import {
+    DivBusiness, BoxBusiness, CircleImg, SectionCategory, SideBar, PageAtual,
+    FilterSection, ComerciosSection, Container
+} from "./style"
+import { FaMapMarkerAlt } from "react-icons/fa"
+import { BiFilterAlt } from "react-icons/bi"
+import { Link } from "react-router-dom"
+
+export default function SubCategorias() {
+    return (
+        <>
+            <Container>
+                <SideBar>
+                    <ul>
+                        <PageAtual><FaMapMarkerAlt />Mercado</PageAtual>
+                        <li>Mercado</li>
+                        <li>Mercado</li>
+                        <li>Mercado</li>
+                        <li>Mercado</li>
+                        <li>Mercado</li>
+                        <li>Mercado</li>
+                        <li>Mercado</li>
+                        <li>Mercado</li>
+                        <li>Mercado</li>
+                        <li>Mercado</li>
+                        <li>Mercado</li>
+                        <li>Mercado</li>
+                        <li>Mercado</li>
+                        <li>Mercado</li>
+                        <li>Mercado</li>
+                    </ul>
+                </SideBar>
+
+                <SectionCategory>
+                    <FilterSection>
+                        <h3>Mercados em Registro</h3>
+                        <p> <BiFilterAlt size={20} />Classificar por: Mais Procurados</p>
+                    </FilterSection>
+
+                    {/* seção para aparecer 2 comercios  */}
+                    <ComerciosSection>
+
+                        <DivBusiness>
+                            <Link to="/comercios">
+                                <BoxBusiness>
+                                    <CircleImg>
+
+                                    </CircleImg>
+                                </BoxBusiness>
+                            </Link>
+                            <h3>Spani Atacadista</h3>
+                            <p>Aberto das 09h até 23h</p>
+                        </DivBusiness>
+
+
+                        <DivBusiness>
+                            <BoxBusiness>
+                                <CircleImg>
+
+                                </CircleImg>
+                            </BoxBusiness>
+                            <h3>aaaaaa</h3>
+                            <p>bbbbbb</p>
+                        </DivBusiness>
+
+                    </ComerciosSection>
+
+                    
+
+                </SectionCategory>
+            </Container>
+        </>
+    )
+}
