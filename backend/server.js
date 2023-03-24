a8d26e521a4f24d5921612687dbd1062554027e7
Asher <95467761+ashern000@users.noreply.github.com>
2023-03-24 20:32:05 -0300

    Update server.js
    Code comments

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,19 @@ const ConnectToDB = require("./src/database/conn")
 const rotaUsuario = require("./src/routes/usuario.route")
 const rotaEmpresa = require("./src/routes/empresa.route")
 
+// Configure Express for read JSON
 app.use(express.json())
-
 app.use(cors())
-
 app.use(express.urlencoded({extended: false}))
 
 app.use("/login",rotaUsuario)
 
 app.use("/empresas",rotaEmpresa)
 
+
+// Connection to Database
 ConnectToDB()
 
+
+//Server running
 app.listen(4041,() => console.log("Rodando na porta 4041"))
