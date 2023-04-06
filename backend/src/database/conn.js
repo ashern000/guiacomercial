fe3859e9d9562eca7f1724e566e8961edd2963fb
Asher <95467761+ashern000@users.noreply.github.com>
2023-04-06 16:25:29 -0300

    Add files via upload

--- backend/src/database/conn.js
+++ backend/src/database/conn.js
@@ -6,7 +6,7 @@ import mongoose from "mongoose";
 
 export const ConnectToDB = async()=>{
     try{
-       await mongoose.connect(`mongodb://${process.env.MONGO_URI}`).then(() => console.log("Conectado!"))
+       await mongoose.connect(`mongodb://${process.env.MONGO_URI}/comart`).then(() => console.log("Conectado!"))
     }catch(error){
         console.log(error)
     }
