54ba23ce0f2d9f884192ea3af011a53a310bb79d
Asher Novelli <asherndebortoli@gmail.com>
2023-05-28 16:57:03 -0300

    pequenas mudanças

--- startApp.bat
+++ startApp.bat
@@ -23,6 +23,8 @@
 @chcp 65001
 @echo "Olá, esse script inicia a aplicação toda, click para continuar!"
 @pause
+docker compose down
+
 docker compose up -d
 docker exec -it mongo1 mongosh --eval "rs.initiate({_id:'comartSet', members: [{_id:0, host: 'mongo1'},{_id:1, host: 'mongo2'},{_id:2, host: 'mongo3'}]})"
 docker exec -it mongo1 mongosh --eval "rs.status()"
