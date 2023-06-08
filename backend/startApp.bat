03b64f5acacc1bbe5cdd95cb9122ccac3351de8b
Asher <95467761+ashern000@users.noreply.github.com>
2023-06-07 23:46:21 -0300

    Update startApp.bat

--- backend/startApp.bat
+++ backend/startApp.bat
@@ -19,14 +19,16 @@
 :: v1.0, 01/05/2023, Asher: ::
 :: Criação do script para iniciar a aplicação de maneira mais fácil. ::
 :: -------------------- EXECUÇãO ---------------------- ::
+
+
 @title 'Start App'
-@chcp 65001
 @echo "Olá, esse script inicia a aplicação toda, click para continuar!"
 @pause
 docker compose down
-
 docker compose up -d
-docker exec -it mongo1 mongosh --eval "rs.initiate({_id:'comartSet', members: [{_id:0, host: 'mongo1'},{_id:1, host: 'mongo2'},{_id:2, host: 'mongo3'}]})"
-docker exec -it mongo1 mongosh --eval "rs.status()"
-npm i 
-npm run dev
+@echo "esperando a configuração do replica set" 
+@timeout -t 10 /nobreak 
+@docker exec -it mongo1 mongosh --eval "rs.initiate({_id:'comartSet', members: [{_id:0, host: 'mongo1'},{_id:1, host: 'mongo2'},{_id:2, host: 'mongo3'}]})"
+@echo status of replica set
+@docker exec -it mongo1 mongosh --eval "rs.status()"
+@npm i && npm run dev
